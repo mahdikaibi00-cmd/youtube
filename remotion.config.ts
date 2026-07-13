7fee353bc7dbf5157283ea8cebf4c67ae444a306
Mahdi Kaibi <mahdikaibi00@gmail.com>
2026-07-13 11:07:01 +0100

    Remove obsolete Config.setPuppeteerTimeout API to fix Remotion v4 crash

--- remotion.config.ts
+++ remotion.config.ts
@@ -13,5 +13,4 @@ Config.setHardwareAcceleration('if-possible');
 
 // Quality optimizations (CRF is INCOMPATIBLE with hardware acceleration - use bitrate instead)
 // Config.setCrf(16); // DISABLED - breaks NVENC
-Config.setPixelFormat('yuv420p');Config.setPuppeteerTimeout(120000);
-Config.setDelayRenderTimeoutInMilliseconds(120000);
+Config.setPixelFormat('yuv420p');Config.setDelayRenderTimeoutInMilliseconds(120000);
